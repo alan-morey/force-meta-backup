e6423e1ab231a52ef851101afa3f706ce49ae769
Alan Morey <alan.morey@globalrelay.net>
2023-03-24 16:07:37 -0700

    Update to Salesforce Platform API v57

--- force-meta-backup.groovy
+++ force-meta-backup.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
-@Grab(group='com.force.api', module='force-partner-api', version='56.1.0')
-@Grab(group='com.force.api', module='force-metadata-api', version='56.1.0')
+@Grab(group='com.force.api', module='force-partner-api', version='57.0.0')
+@Grab(group='com.force.api', module='force-metadata-api', version='57.0.0')
 
 import com.sforce.soap.metadata.FileProperties
 import com.sforce.soap.metadata.ListMetadataQuery
