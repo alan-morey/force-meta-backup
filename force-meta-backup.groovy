dbcec47d481e4eaba8317c1d5a4467db283752b3
Alan Morey <morey.alan@gmail.com>
2025-10-27 15:58:11 -0700

    Support Winter '26 (v65)

--- force-meta-backup.groovy
+++ force-meta-backup.groovy
@@ -1,5 +1,5 @@
-@Grab(group='com.force.api', module='force-partner-api', version='64.0.3')
-@Grab(group='com.force.api', module='force-metadata-api', version='64.0.3')
+@Grab(group='com.force.api', module='force-partner-api', version='65.0.0')
+@Grab(group='com.force.api', module='force-metadata-api', version='65.0.0')
 
 import com.sforce.soap.metadata.FileProperties
 import com.sforce.soap.metadata.ListMetadataQuery
